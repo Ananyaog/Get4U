8131f6c059b31ca5230c6d44002fcfd99c23ad14
Ananyaog <pandeyananya848@gmail.com>
2025-09-27 19:03:50 +0530

    Initial commit

---
+++ src/main/java/com/devops/Get4U/Get4UApplication.java
@@ -0,0 +1,13 @@
+package com.devops.Get4U;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Get4UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Get4UApplication.class, args);
+	}
+
+}
