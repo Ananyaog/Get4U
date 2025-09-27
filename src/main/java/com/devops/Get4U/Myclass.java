34ee319fe66f71360336dbed45c35d11e45c4e87
Ananyaog <pandeyananya848@gmail.com>
2025-09-27 19:35:24 +0530

    Create Myclass.java

---
+++ src/main/java/com/devops/Get4U/Myclass.java
@@ -0,0 +1,16 @@
+package com.devops.Get4U;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class Myclass 
+{
+ @GetMapping("hello")
+ public String Class() {
+     return ("Hello there !");
+ }
+ 
+}
